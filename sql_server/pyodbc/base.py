18dd091032ad3f326deb9b2b81f4abca55729d14
Jorge Filipe <jf@hiri.ai>
2019-08-28 18:22:56 +0100

    Removed tz info from datetime

--- sql_server/pyodbc/base.py
+++ sql_server/pyodbc/base.py
@@ -52,7 +52,7 @@ def handle_datetimeoffset(dto_value):
     tweaked = [tup[i] // 100 if i == 6 else tup[i] for i in range(len(tup))]
     date_string = "{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}.{:07d}{:+03d}:{:02d}".format(*tweaked)
     date_obj = parse(date_string)
-
+    date_obj = date_obj.replace(tzinfo=None)
     return date_obj
 
 
